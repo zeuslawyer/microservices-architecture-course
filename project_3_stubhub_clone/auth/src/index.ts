9c76f5d4fdcde8f3764bbfc4ca655eb1796299ac
zeuslawyer <zubin.pratap@gmail.com>
2020-06-01 16:59:44 +1000

    working with post to /users/signup

--- project_3_stubhub_clone/auth/src/index.ts
+++ project_3_stubhub_clone/auth/src/index.ts
@@ -1,24 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express"
+import bodyParser from "body-parser"
 
-import { currentUserRouter } from './routes/currentUser';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+import { currentUserRouter } from "./routes/currentUser"
+import { signinRouter } from "./routes/signin"
+import { signoutRouter } from "./routes/signout"
+import { signupRouter } from "./routes/signup"
 
-const PORT = 3010;
-const app = express();
-app.use(bodyParser.json());
+const PORT = 3010
+const app = express()
+app.use(bodyParser.json())
 
-app.get('/test-path', (req, res) => {
-  res.json({});
-});
+app.get("/test-path", (req, res) => {
+  res.json({})
+})
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signupRouter)
+app.use(signoutRouter)
 
 app.listen(PORT, () => {
-  console.info('Auth Service Listening On Port', PORT);
-});
+  console.info("Auth Service Listening On Port", PORT)
+})
--- project_3_stubhub_clone/auth/src/routes/signup.ts
+++ project_3_stubhub_clone/auth/src/routes/signup.ts
@@ -1,13 +1,28 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
-const router = express.Router();
+import express, { Request, Response } from "express"
+import { body, validationResult } from "express-validator"
+const router = express.Router()
 
-// middleware array
-const validation = [body('email').isEmail().withMessage('Email must be valid')];
+// middleware validation array of funcs
+const validation = [
+  body("email").isEmail().withMessage("Email must be valid."),
+  body("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage(
+      "Please enter a valid password between 4 and 20 characters long."
+    )
+]
 
-router.post('/api/users/signup', validation, (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  res.send(`Sign up service received GET request on the path ${req.path}`);
-});
+router.post("/api/users/signup", validation, (req: Request, res: Response) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array())
+  }
 
-export { router as signupRouter }; // rename router
+  const { email, password } = req.body
+  res.send(
+    `Sign up service received POST request on the path ${req.path} from ${email}`
+  )
+})
+
+export { router as signupRouter } // rename router
