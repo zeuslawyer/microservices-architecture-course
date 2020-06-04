52d867a4ca6492f295eeae0aae4ea2c6bb612769
zeuslawyer <zubin.pratap@gmail.com>
2020-06-04 18:50:33 +1000

    update errors logic. working

--- project_3_stubhub_clone/auth/src/Errors/RequestValidationError.ts
+++ project_3_stubhub_clone/auth/src/Errors/RequestValidationError.ts
@@ -1,11 +1,24 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
-  constructor(public errors: ValidationError[]) {
+  statusCode = 400;
+  errors: ValidationError[];
+
+  constructor(errors: ValidationError[]) {
     super();
+    this.errors = errors;
 
     // as we are extending a base class included in the language...
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(e => {
+      return {
+        message: e.msg,
+        field: e.param
+      };
+    });
   }
 }
 
--- project_3_stubhub_clone/auth/src/middleware/errorHandler.ts
+++ project_3_stubhub_clone/auth/src/middleware/errorHandler.ts
@@ -4,41 +4,22 @@ import { Request, Response, NextFunction } from "express";
 import { DatabaseConnectionError } from "../Errors/DatabaseConnectionError";
 import { RequestValidationError } from "../Errors/RequestValidationError";
 
-export interface Err {
-  message: string;
-  field?: string;
-}
-export interface AppError {
-  errors: Err[]; // errors is an array of error objects
-}
-
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof DatabaseConnectionError) {
-    const error: AppError = {
-      errors: [{ message: err.reason }]
-    };
-
-    return res.status(500).send(error as AppError);
+  if (err instanceof RequestValidationError) {
+    return res.status(400).send({ errors: err.serializeErrors() });
   }
 
-  if (err instanceof RequestValidationError) {
-    const formattedErrors: Err[] = err.errors.map(e => {
-      return {
-        message: e.msg,
-        field: e.param
-      };
-    });
-
-    return res.status(400).send({ errors: formattedErrors } as AppError);
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   // else
   return res
     .status(400)
-    .send({ errors: [{ message: "Something went wrong. " }] } as AppError);
+    .send({ errors: [{ message: "Something went wrong. " }] });
 };
