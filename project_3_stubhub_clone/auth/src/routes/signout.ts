52b582c020a3a38690fcb9dc1d7252062b847646
zeuslawyer <zubin.pratap@gmail.com>
2020-06-08 17:54:13 +1000

    interim

--- project_3_stubhub_clone/auth/src/routes/signout.ts
+++ project_3_stubhub_clone/auth/src/routes/signout.ts
@@ -6,7 +6,7 @@ const router = express.Router();
  * tell the browser to dump the cookie with its jwt, in the browser.
  * Destroys the session.
  */
-router.post("/api/users/signout", async (req: Request, res: Response) => {
+router.get("/api/users/signout", async (req: Request, res: Response) => {
   req.session = null; // see cookie-sesions docs for destroying session
 
   res.sendStatus(200);
