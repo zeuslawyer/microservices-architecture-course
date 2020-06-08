a274529ea7fa7beaa33db2efeeb7ccda9b426873
zeuslawyer <zubin.pratap@gmail.com>
2020-06-08 18:14:27 +1000

    implement middleware for setting current user on req

--- project_3_stubhub_clone/auth/src/middleware/setCurrentUser.ts
+++ project_3_stubhub_clone/auth/src/middleware/setCurrentUser.ts
@@ -5,13 +5,24 @@ interface UserPayload {
   id: string;
   email: string;
 }
+
+// extend the current type definition of the Request object to include the payload
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 export const setCurrentUser = (req: Request, res: Response, next: NextFunction) => {
   if (req.session!.jwt) {
     try {
-      const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
-
+      const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!) as UserPayload;
       req.currentUser = payload;
+      next();
     } catch (e) {
+      console.info("No user session available: ", e.message);
       next();
     }
   } else {
--- project_3_stubhub_clone/auth/src/routes/currentUser.ts
+++ project_3_stubhub_clone/auth/src/routes/currentUser.ts
@@ -1,29 +1,18 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
+import { setCurrentUser } from "../middleware/setCurrentUser";
+
 const router = express.Router();
 
 /**
  * Route for react app front end to hit to check if a current user is authentication and if so, authorized.
  * Route will check if jwt is on the req.sessions cookie object.
  */
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  // check if user is logged in/ has a session
-  const userSessionExists = !!req.session?.jwt;
-  if (!userSessionExists)
-    return res.send({
-      currentUser: null
-    });
+router.get("/api/users/currentuser", setCurrentUser, (req: Request, res: Response) => {
+  // req.currentUser is set by the middleware.  Can be undefined if not logged in.abs
 
-  // check jwt is intact
-  try {
-    const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({
-      currentUser: null
-    });
-  }
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter }; // rename router
