6e77c1fc6df253007642ed62d967ea84ba588637
zeuslawyer <zubin.pratap@gmail.com>
2020-06-08 16:27:12 +1000

    wire up signin logic w validation

--- project_3_stubhub_clone/auth/src/routes/signin.ts
+++ project_3_stubhub_clone/auth/src/routes/signin.ts
@@ -1,9 +1,30 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../Errors/RequestValidationError";
 
 const router = express.Router();
 
-router.post("/api/users/signin", async (req: Request, res: Response) => {
-  res.send(`sign in service received POST request on the path ${req.path}`);
-});
+const validation = [
+  body("email")
+    .isEmail()
+    .withMessage("Please provide a valid email address for signing in."),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Please provide your password to sign in.")
+];
+router.post(
+  "/api/users/signin",
+  validation,
+  async (req: Request, res: Response) => {
+    // handle validation errors on input
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    res.status(200);
+  }
+);
 
 export { router as signinRouter }; // rename router
