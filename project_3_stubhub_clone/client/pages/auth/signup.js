d63eabf5e6090e90e4aa13f79d4efa7c4a113b98
zeuslawyer <zubin.pratap@gmail.com>
2020-06-13 16:40:06 +1000

    signup form handle submit and error

--- project_3_stubhub_clone/client/pages/auth/signup.js
+++ project_3_stubhub_clone/client/pages/auth/signup.js
@@ -1,28 +1,50 @@
 import React from "react";
-
+import axios from "axios";
 const Signup = () => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const submitForm = event => {
-    event.preventDefault();
+  const [error, setError] = React.useState("");
 
-    console.log({
-      email,
-      password
-    });
+  const submitForm = async event => {
+    event.preventDefault();
+    const PATH = "/api/users/signup";
+    let res;
+    try {
+      res = await axios.post(PATH, {
+        email,
+        password
+      });
+      console.log("RESPONSE", res);
+    } catch (error) {
+      if (error.response) {
+        const err = error.response.data.errors[0];
+        setError(err.message);
+      }
+    } finally {
+      setPassword("");
+      setEmail("");
+    }
   };
   return (
-    <form onSubmit={submitForm}>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input className="form-control" value={email} onChange={e => setEmail(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input className="form-control" type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </div>
-      <button className="btn btn-primary">Sign up</button>
-    </form>
+    <>
+      <form onSubmit={submitForm}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input className="form-control" value={email} onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            className="form-control"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Sign up</button>
+      </form>
+      {error ? <p style={{ color: "red" }}> {error}</p> : null}
+    </>
   );
 };
 
