f9b3bf232113cba9603c0b44c8f49bd3f95bf1ef
zeuslawyer <zubin.pratap@gmail.com>
2020-06-21 12:32:21 +1000

    refactor axios to make custom axios object

---
+++ project_3_stubhub_clone/client/api/customAxios.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const customAxios = ({ req }) => {
+  const isServerSide = typeof window === "undefined";
+  const namespace = `ingress-nginx`; // obtain with kubectl get namespace
+  const srvName = `ingress-nginx-controller`; // obtain with kubectl get services -n <ingress-nginx namespace>
+  const suffix = `svc.cluster.local`;
+
+  if (isServerSide) {
+    // return axios object
+    return axios.create({
+      baseURL: `http://${srvName}.${namespace}.${suffix}`,
+      headers: req.headers
+    });
+  } else {
+    // browser side
+    return axios.create({
+      baseURL: "/" // empty base url - but optional. can be left off
+      // no need for headers as this is coming entirely browser side and getInitialProps wont receive a headers object. it will come directly from browser
+    });
+  }
+};
+
+export default customAxios;
--- project_3_stubhub_clone/client/pages/index.js
+++ project_3_stubhub_clone/client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import customAxios from "../api/customAxios";
 
 const LandingPage = ({ currentUser }) => {
   console.log("props", currentUser);
@@ -7,30 +8,13 @@ const LandingPage = ({ currentUser }) => {
 
 // custom NextJs API to do some work while NextJs is building up the HTML to render back
 // often done to fetch data for the initial / first load of page
-
 // this function is run by the SERVER on initial load, reload etc,  and in the BROWSER/CLIENT when navigating between pages of the client app .
-LandingPage.getInitialProps = async ({ req }) => {
-  // determine if we're on server or browser
-  const isServerSide = typeof window === "undefined";
+LandingPage.getInitialProps = async context => {
+  const axiosClient = customAxios(context);
 
-  const namespace = `ingress-nginx`; // obtain with kubectl get namespace
-  const srvName = `ingress-nginx-controller`; // obtain with kubectl get services -n <ingress-nginx namespace>
-  const suffix = `svc.cluster.local`;
-
-  // handle url routing logic depending on environment
-  const options = {};
-  let baseUrl;
   const path = "/api/users/currentuser";
-  if (isServerSide) {
-    baseUrl = `http://${srvName}.${namespace}.${suffix}` + path;
-    // set options with a Host header so that the ingress-nginx-srv service knows which domain this request is coming from, since its not coming from browser
-    options.headers = req.headers; // pass along the headers, which includes cookie and domain (so that ingress-nginx-srv can inspect the incoming request domain)
-  } else {
-    baseUrl = path;
-  }
-  console.log("THE URL", baseUrl);
-
-  const response = await axios.get(baseUrl, options);
-  return response.data; // the current user object to be passed into component as props
+  const response = await axiosClient.get(path);
+  return response.data;
 };
+
 export default LandingPage;
