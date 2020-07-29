99fa73d509c9569fd7d77237b156d69586f37b12
Zubin Pratap <zubin.pratap@gmail.com>
2020-07-29 10:06:06 +1000

    added index message

--- project_3_stubhub_clone/auth/src/index.ts
+++ project_3_stubhub_clone/auth/src/index.ts
@@ -7,14 +7,17 @@ const PORT = 3000;
 const init = async () => {
   // check env vars
   if (!process.env.JWT_KEY) throw new Error(" MISSING ENV VAR JWT_KEY ");
-  if (!process.env.MONGO_URI) throw new Error(" MISSING ENV VAR MONGO_URI in AUTH ");
+  if (!process.env.MONGO_URI)
+    throw new Error(" MISSING ENV VAR MONGO_URI in AUTH ");
+
+  console.log(" ENV VARS LOADED ...");
 
   // mongoose
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
   } catch (error) {
     console.error("AUTH ERROR:   Failed to Connect to Database");
